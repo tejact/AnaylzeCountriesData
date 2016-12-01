63efa2d9af8713a6f100a9ae80a155e721920043
teja.ct <teja.tummalapalli@gmail.com>
2016-12-01 09:34:42 -0500

    Added description in main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String args[]) {
         System.out.println("Hello..Welcome to Hibernate");
+		System.out.println("This project is used to anlayze the data of the countires");		
     }
 }
 
