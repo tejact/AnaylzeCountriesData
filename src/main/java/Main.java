c9416057d5b72b1033887e904eb883528dfed269
tejact <teja.tummalapalli@gmail.com>
2016-12-05 20:56:57 -0500

    Contry builder and Hibernate Config in Code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,53 @@
+import com.tejatummalapalli.analyzeCountries.model.Country;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.List;
+
 public class Main {
+    private final SessionFactory sessionFactory = buildSessionFactory();
+
+    private SessionFactory buildSessionFactory() {
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
     public static void main(String args[]) {
         System.out.println("Hello..Welcome to Hibernate");
+
+
+        Country dummyCountry = new Country.CountryBuilder()
+                                    .withName("India2")
+                                    .withAdultLiteracyRate(90l)
+                                    .withAdultLiteracyRate(80l)
+                                    .build();
+
+        Main main = new Main();
+
+        List allCountries = main.getAllCountries();
+
+                allCountries
+                .stream().
+                forEach(System.out::println);
+
+
+    }
+
+    public List<Country> getAllCountries() {
+        Session session = sessionFactory.openSession();
+
+        Criteria criteria = session.createCriteria(Country.class);
+
+        List<Country> countries = criteria.list();
+
+
+        session.close();
+
+        return countries;
     }
 }
 
--- src/main/java/com/tejatummalapalli/analyzeCountries/model/Country.java
+++ src/main/java/com/tejatummalapalli/analyzeCountries/model/Country.java
@@ -1,10 +1,31 @@
 package com.tejatummalapalli.analyzeCountries.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Country {
+    @Id
+    @Column(length = 3)
     String code;
+    @Column(length=32)
     String name;
+    @Column(columnDefinition = "decimal",precision = 11,scale = 8)
     long internetUsers;
+    @Column(columnDefinition = "decimal",precision = 11,scale = 8)
     long adultLiteracyRate;
+
+    //Default Constructor for JPA's
+    public Country() {
+
+    }
+
+    public Country(CountryBuilder countryBuilder) {
+          this.name = countryBuilder.name;
+          this.internetUsers = countryBuilder.internetUsers;
+          this.adultLiteracyRate = countryBuilder.adultLiteracyRate;
+    }
 
     public String getCode() {
         return code;
@@ -36,5 +57,30 @@ public class Country {
 
     public void setAdultLiteracyRate(long adultLiteracyRate) {
         this.adultLiteracyRate = adultLiteracyRate;
+    }
+
+    public static class CountryBuilder {
+        String name;
+        long internetUsers;
+        long adultLiteracyRate;
+
+        public CountryBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public CountryBuilder withInternetUsers(long internetUsers) {
+            this.internetUsers = internetUsers;
+            return this;
+        }
+
+        public CountryBuilder withAdultLiteracyRate(long adultLiteracyRate) {
+            this.adultLiteracyRate = adultLiteracyRate;
+            return this;
+        }
+
+        public Country build() {
+            return new Country(this);
+        }
     }
 }
